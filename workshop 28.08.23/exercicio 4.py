ce3608fb605277628378a06fb5f969441ed628de
ewellyn <129416755+ewemf@users.noreply.github.com>
2023-08-28 18:49:46 -0300

    Delete workshop 28.08.23/exercicio 4.py

--- workshop 28.08.23/exercicio 4.py
+++
@@ -1,7 +0,0 @@
-# crie um programa que leia uma temperatura em celsius e a converta para fahrenheit
-
-celsius = int(input('Digite a temperatura em Celsius: '))
-
-fahrenheit = (celsius * 1.8) + 32
-
-print('A temperatura em Fahrenheit fica: {}'.format(fahrenheit))
