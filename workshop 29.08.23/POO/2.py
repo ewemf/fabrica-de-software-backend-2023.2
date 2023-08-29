5129069584896c3edb88f070fa7e4a636984bca8
ewellyn <129416755+ewemf@users.noreply.github.com>
2023-08-29 18:35:05 -0300

    Update 2.py

--- workshop 29.08.23/POO/2.py
+++ workshop 29.08.23/POO/2.py
@@ -10,5 +10,5 @@ class ContaBancária ():
     def contabancaria(self):
         print (f'Oi, meu titular é {self.titular}, tenho {self.saldo} reais de saldo e esse é um dos métodos para depositar dinheiro: {self.metodosDep}; e esse para sacar dinheiro: {self.metodosSac}.')
 
-pessoa = ContaBancária('Ewellyn', 200, 'Depósito', 'Saque')
+pessoa = ContaBancária('Ewellyn', 200, 'Depósito Bancário', 'Saque Digital')
 pessoa.contabancaria()
