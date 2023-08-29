4f7c749801fb90eb602b856dfbf0ad466b29570b
ewellyn <129416755+ewemf@users.noreply.github.com>
2023-08-29 18:44:45 -0300

    Update 3.py

--- workshop 29.08.23/POO/3.py
+++ workshop 29.08.23/POO/3.py
@@ -1,2 +1,13 @@
 #Classe Carro Crie uma classe chamada Carro com atributos como marca, modelo e ano. Crie um método para acelerar o carro.
 
+class Carro():
+    def __init__(self, marca, modelo, ano):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+    
+    def carro(self):
+        print (f'MARCA DO CARRO: {self.marca}, MODELO: {self.modelo}, ANO: {self.ano}.')
+
+carro = Carro('Hyundai HB20', 'Comfort Automático', 2023)
+carro.carro()
