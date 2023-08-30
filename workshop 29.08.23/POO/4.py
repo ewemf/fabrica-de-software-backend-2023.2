78a00490312b0cd5a294952bbd4384bfd43e0187
ewellyn <129416755+ewemf@users.noreply.github.com>
2023-08-30 14:33:35 -0300

    Update 4.py

--- workshop 29.08.23/POO/4.py
+++ workshop 29.08.23/POO/4.py
@@ -25,3 +25,26 @@ raio = 3
 
 print("Área do RETÂNGULO:", retangulo.area(largura, altura))
 print("Área do CÍRCULO:", circulo.area(raio))
+
+# EXEMPLO FEITO NA SALA:
+
+class Garrafa:
+    def __init__(self, tamanho, cor, marca):
+        self.tamanho = tamanho
+        self.cor = cor
+        self.marca = marca
+
+    def __str__(self):
+        return f'tamanho: {self.tamanho}, marca: {self.marca}'
+
+class GarrafaDeVidro(Garrafa):
+    def quebrar(self):
+        print('crack')
+
+garrafa = Garrafa(200, 'preto', 'Stanley')
+
+garrafa_de_vidro = GarrafaDeVidro(200, 'transparente', 'vidro')
+
+a = garrafa.__str__()
+print(a)
+print(garrafa_de_vidro.__str__())
