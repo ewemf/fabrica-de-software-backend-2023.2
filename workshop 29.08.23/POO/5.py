6df744490402bd667cc5427bd0d3c3ac68b1e489
ewellyn <129416755+ewemf@users.noreply.github.com>
2023-08-30 15:21:55 -0300

    Update 5.py

--- workshop 29.08.23/POO/5.py
+++ workshop 29.08.23/POO/5.py
@@ -1,28 +1,28 @@
 # polimorfism Crie uma classe base chamada Animal com métodos para emitir um som e mostrar informações gerais. Crie subclasses como Cachorro e Gato que herdam de Animal e implementam seus próprios métodos para emitir sons específicos.
 
 class Animal:
-    def emitir_som(self):
+    def som(self):
         pass
     
-    def info_gerais(self):
+    def info(self):
         pass
 
 class Cachorro(Animal):
-    def emitir_som(self):
-        return "O cachorro faz 'au au'"
+    def som(self):
+        return "Som emitido pelo CACHORRO: 'AU AU'"
     
-    def info_gerais(self):
-        return "Cachorro: Animal doméstico fiel e leal"
+    def info(self):
+        return "Informações gerais do CACHORRO: Animal doméstico fiel e amoroso"
 
 class Gato(Animal):
-    def emitir_som(self):
-        return "O gato faz 'miau'"
+    def som(self):
+        return "Som emitido pelo GATO: 'MIAU'"
     
-    def info_gerais(self):
-        return "Gato: Animal independente e carinhoso"
+    def info(self):
+        return "Informações gerais do GATO: Animal independente e carinhoso"
 
 animais = [Cachorro(), Gato()]
 
 for animal in animais:
-    print(animal.emitir_som())
-    print(animal.info_gerais())
+    print(animal.som())
+    print(animal.info())
