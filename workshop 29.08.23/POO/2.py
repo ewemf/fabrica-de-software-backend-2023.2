96e49ad90745c5c8cb3332a40bed6e5a78fed346
ewellyn <129416755+ewemf@users.noreply.github.com>
2023-08-30 14:23:30 -0300

    Update 2.py

--- workshop 29.08.23/POO/2.py
+++ workshop 29.08.23/POO/2.py
@@ -1,14 +1,24 @@
 # Classe Conta Bancária Crie uma classe chamada ContaBancaria que tenha atributos como titular, saldo e métodos para depositar e sacar dinheiro.
 
 class ContaBancária ():
-    def __init__(self, titular, saldo, metodosDep, metodosSac):
+    def __init__(self, titular, saldo):
         self.titular = titular
         self.saldo = saldo
-        self.metodosDep = metodosDep
-        self.metodosSac = metodosSac
-    
-    def contabancaria(self):
-        print (f'Titular: {self.titular}; SALDO: {self.saldo}; Método para depositar dinheiro: {self.metodosDep}; Método para sacar dinheiro: {self.metodosSac}.')
 
-pessoa = ContaBancária('Ewellyn', 200, 'Depósito Bancário', 'Saque Digital')
-pessoa.contabancaria()
+    def mostrar_saldo(self):
+        print(f'Seu saldo é: {self.saldo}')
+    def depositar(self, dinheiro):
+        self.saldo += dinheiro
+    
+    def sacar_dinheiro(self, valor):
+        if valor > self.saldo:
+            print('voce nao pode sacar.')
+        else:
+            self.saldo -= valor
+
+guilherme = ContaBancária('guilherme', 0)
+guilherme.depositar(50)
+guilherme.mostrar_saldo()
+
+guilherme.sacar_dinheiro(20)
+guilherme.mostrar_saldo()
